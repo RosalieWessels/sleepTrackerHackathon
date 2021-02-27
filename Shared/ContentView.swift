f604df45ffb5d94f695b439615fc9c3f0a1bf65a
Jamesduongrx <jamtube47@gmail.com>
2021-02-27 14:03:06 -0800

    Bar Graph Style - James

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,10 +12,17 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                //LineView(data: [8,9,7,7.5,5,8,10], title: "My Sleep", legend: "Hours")
-                BarChartView(data: ChartData(values: [("Monday",8), ("Tuesday",7), ("Wednesday",6), ("Thursday",7.5), ("Today",9)]), title: "My Sleep Hours", legend: "Per day", form: ChartForm.extraLarge)
-                Text("This is our sleep tracker app!")
-            }
+                Spacer( )                //LineView(data: [8,9,7,7.5,5,8,10], title: "My Sleep", legend: "Hours")
+                let chartStyle = ChartStyle(backgroundColor: Color.black, accentColor: Colors.GradientNeonBlue, secondGradientColor: Colors.OrangeEnd, textColor: Color.white, legendTextColor: Color.white, dropShadowColor: Color.white)
+    
+                BarChartView(data: ChartData(values: [("Monday",8), ("Tuesday",7), ("Wednesday",6), ("Thursday",7.5), ("Today",9)]), title: "My Sleep Hours", legend: "Per day", style: chartStyle, form: ChartForm.extraLarge)
+                Spacer()
+                BarChartView(data: ChartData(values: [("Monday",8), ("Tuesday",7), ("Wednesday",6), ("Thursday",7.5), ("Today",9)]), title: "My Sleep Rating", legend: "Per day", style: chartStyle, form: ChartForm.extraLarge)
+                Spacer()
+            }.background(Image("background").resizable().scaledToFill().clipped())
+            .edgesIgnoringSafeArea([.top])
+            
+            
             .navigationBarTitle("Sleep Tracker App", displayMode: .inline)
             .navigationBarItems(trailing: NavigationLink(destination: LogSleepView()) {
                 Text("Log Sleep")

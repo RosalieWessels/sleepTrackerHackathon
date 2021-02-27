11f3e084c65b7e275a4ccdf1adee7c63a0edca5f
Jamesduongrx <jamtube47@gmail.com>
2021-02-27 11:00:02 -0800

    Change title to "testing"

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             
-            Text("This is our sleep tracker app!")
+            Text("Testing")
             
                 .navigationBarTitle("Sleep Tracker App", displayMode: .inline)
                 .navigationBarItems(trailing: NavigationLink(destination: LogSleepView()) {

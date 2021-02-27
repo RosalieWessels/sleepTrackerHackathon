eaf158b3b86024bc20f84ff388bd897f8e9affad
RosalieWessels <rosalie.wessels@yahoo.com>
2021-02-27 09:05:24 -0800

    Initial Commit

---
+++ Shared/sleepTrackerHackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleepTrackerHackathonApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct sleepTrackerHackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

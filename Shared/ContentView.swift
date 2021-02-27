739d8e028fecd30f81ac0c47c23c62bb98442d3c
RosalieWessels <rosalie.wessels@yahoo.com>
2021-02-27 11:07:51 -0800

    Added Bar Graph

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,17 +6,20 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            
-            Text("Testing")
-            
-                .navigationBarTitle("Sleep Tracker App", displayMode: .inline)
-                .navigationBarItems(trailing: NavigationLink(destination: LogSleepView()) {
-                    Text("Log Sleep")
-                })
+            VStack {
+                //LineView(data: [8,9,7,7.5,5,8,10], title: "My Sleep", legend: "Hours")
+                BarChartView(data: ChartData(values: [("Monday",8), ("Tuesday",7), ("Wednesday",6), ("Thursday",7.5), ("Today",9)]), title: "My Sleep Hours", legend: "Per day", form: ChartForm.extraLarge)
+                Text("This is our sleep tracker app!")
+            }
+            .navigationBarTitle("Sleep Tracker App", displayMode: .inline)
+            .navigationBarItems(trailing: NavigationLink(destination: LogSleepView()) {
+                Text("Log Sleep")
+            })
         }
         
     }

a18de933f64ab1f717ab8546399d4447840459ec
RosalieWessels <rosalie.wessels@yahoo.com>
2021-02-27 14:39:03 -0800

    Added Firebase database and My Sleep Hours graph uses data from Firebase Database

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'sleepTrackerHackathon (iOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for sleepTrackerHackathon (iOS)
+  pod 'Firebase/Firestore'
+  pod 'Firebase/Auth'
+
+end
+
+target 'sleepTrackerHackathon (macOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for sleepTrackerHackathon (macOS)
+  pod 'Firebase/Firestore'
+  pod 'Firebase/Auth'
+
+end
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,22 +7,76 @@
 
 import SwiftUI
 import SwiftUICharts
+import Firebase
 
 struct ContentView: View {
+    
+    //var db: Firestore!
+    @State var db = Firestore.firestore()
+    
+    @State var sleepHourOne = 2
+    @State var sleepHoursData : [Double] = [2.0, 8.0, 7.5, 9.5, 10]
+    @State var chartData = [("Test", 10.0)]
+    
+        
     var body: some View {
         NavigationView {
             VStack {
                 //LineView(data: [8,9,7,7.5,5,8,10], title: "My Sleep", legend: "Hours")
-                BarChartView(data: ChartData(values: [("Monday",8), ("Tuesday",7), ("Wednesday",6), ("Thursday",7.5), ("Today",9)]), title: "My Sleep Hours", legend: "Per day", form: ChartForm.extraLarge)
+                if chartData[0] != ("Test", 10.0) {
+                    BarChartView(data: ChartData(values: chartData), title: "My Sleep Hours", legend: "Per day", form: ChartForm.extraLarge)
+                }
                 Text("This is our sleep tracker app!")
+                Button(action: {
+                    getData()
+                }) {
+                    Text("click")
+                }
+                
             }
+            
             .navigationBarTitle("Sleep Tracker App", displayMode: .inline)
             .navigationBarItems(trailing: NavigationLink(destination: LogSleepView()) {
                 Text("Log Sleep")
             })
+            .onAppear(perform: getData)
         }
         
     }
+    
+
+    
+    func getData() {
+        db.collection("TestPerson").order(by: "Date", descending: true).limit(to: 5).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                    if let sleepHours = document.get("SleepHours") as? Double {
+                        print("SleepHours", sleepHours)
+                        if let sleepDate = document.get("DateString") as? String {
+                            
+                            chartData.append(("\(sleepDate)", Double(sleepHours)))
+                            
+                            print(chartData)
+                            
+                            if chartData.count == 6 && chartData[0] == ("Test", 10.0) {
+                                chartData.remove(at: 0)
+                                print("yes")
+                            }
+                        }
+                        else {
+                            print("Hello")
+                        }
+                        
+                    }
+                }
+            }
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
